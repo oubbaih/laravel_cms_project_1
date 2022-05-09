c0785a54cbccf3666cf03bc8d115bd27f3acc713
lahcen oubbaih <eoubbaih@gmail.com>
2022-05-09 11:31:31 +0100

    uploads images working coorectly

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="{{asset('css/app.css')}}" rel="stylesheet">
-  <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
   {{-- css Links Of SummernoteJs  --}}
   @yield('cssSummernote')
 </head>
@@ -53,7 +53,7 @@
   <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+  <script src="{{asset('js/sb-admin-2.js')}}"></script>
 
   @yield('scripts')
 
